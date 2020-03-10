c980ba003d24f6a6ae9146b21a951179fee9f1d9
TeunStout <teunstout@hotmail.com>
2020-03-10 15:37:11 +0100

    3.2 niet helemaal af

--- UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -1,5 +1,28 @@
 package com.example.userprofile
 
-class CreateProfileActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        initViews()
+    }
+
+    private fun initViews() {
+        btnGallery.setOnClickListener{ onGalleryClick() }
+    }
+
+    private fun onGalleryClick() {
+        val pictureIntent = Intent(Intent.ACTION_PICK)
+        pictureIntent.type = "image/*"
+        startActivityForResult(pictureIntent, GALLERY_REQUEST_CODE)
+    }
 
 }
