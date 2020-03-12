cab10919df9463496312705c8f507ba90f3c4d92
TeunStout <teunstout@hotmail.com>
2020-03-12 11:08:24 +0100

    2 done

--- UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -1,28 +1,15 @@
-import android.annotation.SuppressLint
-import android.content.Intent
+package com.example.userprofile
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.userprofile.R
-import kotlinx.android.synthetic.main.activity_profile.*
 
-const val GALLERY_REQUEST_CODE = 100
 
 class CreateProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        initViews()
-    }
 
-    private fun initViews() {
-        btnGallery.setOnClickListener{ onGalleryClick() }
-    }
-
-    private fun onGalleryClick() {
-        val pictureIntent = Intent(Intent.ACTION_PICK)
-        pictureIntent.type = "image/*"
-        startActivityForResult(pictureIntent, GALLERY_REQUEST_CODE)
     }
 
 }
