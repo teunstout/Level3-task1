9de0cd0daf27acd8c150dce2efd1025b06305496
TeunStout <teunstout@hotmail.com>
2020-03-12 11:35:09 +0100

    3.2

--- UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -1,8 +1,11 @@
 package com.example.userprofile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 
+const val GALLERY_REQUEST_CODE = 100
 
 class CreateProfileActivity : AppCompatActivity() {
 
@@ -10,6 +13,20 @@ class CreateProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        btnGallery.setOnClickListener { onGalleryClick() }
     }
+
+
+    private fun onGalleryClick() {
+        // Create an Intent with action as ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        // Sets the type as image/*. This ensures only components of type image are selected
+        galleryIntent.type = "image/*"
+
+        // Start the activity using the gallery intent
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
 
 }
